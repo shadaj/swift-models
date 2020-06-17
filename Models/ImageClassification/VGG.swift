0c52766d6d57aa2513687debda2e2de85162afc9
Shadaj Laddad <shadaj@users.noreply.github.com>
2020-06-17 11:43:18 -0700

    Port VGG16/19 to new layer API

--- Models/ImageClassification/VGG.swift
+++ Models/ImageClassification/VGG.swift
@@ -13,85 +13,103 @@
 // limitations under the License.
 
 import TensorFlow
+import LayerInit
 
 // Original Paper:
 // "Very Deep Convolutional Networks for Large-Scale Image Recognition"
 // Karen Simonyan, Andrew Zisserman
 // https://arxiv.org/abs/1409.1556
 
-public struct VGGBlock: Layer {
-    var blocks: [Conv2D<Float>] = []
-    var maxpool = MaxPool2D<Float>(poolSize: (2, 2), strides: (2, 2))
+public struct AutoVGGBlock: AutoModule {
+    let featureCounts: (Int, Int, Int, Int)
+    let blockCount: Int
 
     public init(featureCounts: (Int, Int, Int, Int), blockCount: Int) {
-        self.blocks = [Conv2D<Float>(filterShape: (3, 3, featureCounts.0, featureCounts.1),
+        self.featureCounts = featureCounts
+        self.blockCount = blockCount
+    }
+
+    public typealias LayerType = AutoSequenced<AutoSequencedMany<AutoConv2D<Float>>, AutoMaxPool2D<Float>>
+    public lazy var initializeLayer: LayerType = {
+        var blocks = [AutoConv2D<Float>(filterShape: (3, 3),
+            outputChannels: featureCounts.1,
             padding: .same,
             activation: relu)]
+        
         for _ in 1..<blockCount {
-            self.blocks += [Conv2D<Float>(filterShape: (3, 3, featureCounts.2, featureCounts.3),
+            blocks += [AutoConv2D<Float>(filterShape: (3, 3),
+                outputChannels: featureCounts.3,
                 padding: .same,
                 activation: relu)]
         }
+        
+        return AutoSequencedMany(layers: blocks)
+            .then(AutoMaxPool2D<Float>(poolSize: (2, 2), strides: (2, 2)))
+    }()
+}
+
+public struct AutoVGG16: AutoModule {
+    let classCount: Int
+
+    public init(classCount: Int = 1000) {
+        self.classCount = classCount
     }
 
-    @differentiable
-    public func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
-        return maxpool(blocks.differentiableReduce(input) { $1($0) })
-    }
+    public lazy var initializeLayer = {
+        return AutoVGGBlock(featureCounts: (3, 64, 64, 64), blockCount: 2)
+            .then(AutoVGGBlock(featureCounts: (64, 128, 128, 128), blockCount: 2))
+            .then(AutoVGGBlock(featureCounts: (128, 256, 256, 256), blockCount: 3))
+            .then(AutoVGGBlock(featureCounts: (256, 512, 512, 512), blockCount: 3))
+            .then(AutoVGGBlock(featureCounts: (512, 512, 512, 512), blockCount: 3))
+            .then(AutoFlatten())
+            .then(AutoDense(outputSize: 4096, activation: relu))
+            .then(AutoDense(outputSize: 4096, activation: relu))
+            .then(AutoDense(outputSize: classCount))
+    }()
 }
 
 public struct VGG16: Layer {
-    var layer1: VGGBlock
-    var layer2: VGGBlock
-    var layer3: VGGBlock
-    var layer4: VGGBlock
-    var layer5: VGGBlock
-
-    var flatten = Flatten<Float>()
-    var dense1 = Dense<Float>(inputSize: 512 * 7 * 7, outputSize: 4096, activation: relu)
-    var dense2 = Dense<Float>(inputSize: 4096, outputSize: 4096, activation: relu)
-    var output: Dense<Float>
+    var underlying: BuiltAutoLayer<AutoVGG16.InstanceType>
 
     public init(classCount: Int = 1000) {
-        layer1 = VGGBlock(featureCounts: (3, 64, 64, 64), blockCount: 2)
-        layer2 = VGGBlock(featureCounts: (64, 128, 128, 128), blockCount: 2)
-        layer3 = VGGBlock(featureCounts: (128, 256, 256, 256), blockCount: 3)
-        layer4 = VGGBlock(featureCounts: (256, 512, 512, 512), blockCount: 3)
-        layer5 = VGGBlock(featureCounts: (512, 512, 512, 512), blockCount: 3)
-        output = Dense(inputSize: 4096, outputSize: classCount)
+        underlying = AutoVGG16(classCount: classCount).buildModel(inputShape: (224, 224, 3))
     }
 
     @differentiable
     public func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
-        let backbone = input.sequenced(through: layer1, layer2, layer3, layer4, layer5)
-        return backbone.sequenced(through: flatten, dense1, dense2, output)
+        underlying(input)
     }
 }
 
-public struct VGG19: Layer {
-    var layer1: VGGBlock
-    var layer2: VGGBlock
-    var layer3: VGGBlock
-    var layer4: VGGBlock
-    var layer5: VGGBlock
-
-    var flatten = Flatten<Float>()
-    var dense1 = Dense<Float>(inputSize: 512 * 7 * 7, outputSize: 4096, activation: relu)
-    var dense2 = Dense<Float>(inputSize: 4096, outputSize: 4096, activation: relu)
-    var output: Dense<Float>
+public struct AutoVGG19: AutoModule {
+    let classCount: Int
 
     public init(classCount: Int = 1000) {
-        layer1 = VGGBlock(featureCounts: (3, 64, 64, 64), blockCount: 2)
-        layer2 = VGGBlock(featureCounts: (64, 128, 128, 128), blockCount: 2)
-        layer3 = VGGBlock(featureCounts: (128, 256, 256, 256), blockCount: 4)
-        layer4 = VGGBlock(featureCounts: (256, 512, 512, 512), blockCount: 4)
-        layer5 = VGGBlock(featureCounts: (512, 512, 512, 512), blockCount: 4)
-        output = Dense(inputSize: 4096, outputSize: classCount)
+        self.classCount = classCount
+    }
+
+    public lazy var initializeLayer = {
+        return AutoVGGBlock(featureCounts: (3, 64, 64, 64), blockCount: 2)
+            .then(AutoVGGBlock(featureCounts: (64, 128, 128, 128), blockCount: 2))
+            .then(AutoVGGBlock(featureCounts: (128, 256, 256, 256), blockCount: 4))
+            .then(AutoVGGBlock(featureCounts: (256, 512, 512, 512), blockCount: 4))
+            .then(AutoVGGBlock(featureCounts: (512, 512, 512, 512), blockCount: 4))
+            .then(AutoFlatten())
+            .then(AutoDense(outputSize: 4096, activation: relu))
+            .then(AutoDense(outputSize: 4096, activation: relu))
+            .then(AutoDense(outputSize: classCount))
+    }()
+}
+
+public struct VGG19: Layer {
+    var underlying: BuiltAutoLayer<AutoVGG19.InstanceType>
+
+    public init(classCount: Int = 1000) {
+        underlying = AutoVGG19(classCount: classCount).buildModel(inputShape: (224, 224, 3))
     }
 
     @differentiable
     public func callAsFunction(_ input: Tensor<Float>) -> Tensor<Float> {
-        let backbone = input.sequenced(through: layer1, layer2, layer3, layer4, layer5)
-        return backbone.sequenced(through: flatten, dense1, dense2, output)
+        underlying(input)
     }
 }
