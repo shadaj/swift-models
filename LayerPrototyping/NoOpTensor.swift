a69b97c73dbf1113a3900da1e52ba5ee40cbe6b3
Shadaj Laddad <shadaj@users.noreply.github.com>
2020-07-23 15:48:36 -0700

    Disable inlining for no-op tensor

--- LayerPrototyping/NoOpTensor.swift
+++ LayerPrototyping/NoOpTensor.swift
@@ -2,16 +2,20 @@ public struct NoOpTensor: EuclideanDifferentiable, KeyPathIterable {
     @noDerivative let shape: [Int]; // TODO(shadaj): fixed size array
     public typealias TangentVector = Float
 
+    @inline(never)
     public init(shape: [Int]) {
         self.shape = shape
     }
 
     @differentiable
+    @inline(never)
     public func transformByFakeWeights(weights: NoOpTensor) -> NoOpTensor {
         return NoOpTensor(shape: weights.shape)
     }
 
+    @inline(never)
     public mutating func move(along direction: Float) {}
 
+    @inline(never)
     public var differentiableVectorView: Float = 0.0
 }
